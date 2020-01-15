5971205c55034a950b4f33671205dc264a0269b7
DJ Gsus Martinez <djgsus.martinez@gmail.com>
2020-01-15 16:52:44 +0100

    Added MySQL connection

---
+++ Caliz.API/Models/CalizContext.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caliz.API.Models
+{
+    public class CalizContext
+    {
+        public string ConnectionString { get; set; }
+
+        public CalizContext(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Caliz.API/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caliz.API.Models
+{
+    public class User
+    {
+        private User context;
+
+        public int IDUser { get; set; }
+
+        public int IDRutine { get; set; }
+
+        public int IDProgressions{ get; set; }
+        
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+
+        public DateTime Birthdate{ get; set; }
+        public string email { get; set; }
+        public string Goals{ get; set; }
+    }
+}
